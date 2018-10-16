48140cb57916b6c1b479fc497b930b459222cf64
iqbalhasan24 <iqbalhasan.info@gmail.com>
2018-10-17 05:07:20 +0600

    single page design

--- wp-content/themes/news-theme/single.php
+++ wp-content/themes/news-theme/single.php
@@ -1,11 +1,31 @@
 <?php get_header(); ?>
 <div class="container-fluid" style="/*max-width:100%;*/ ">
+	<div class="row each-row">
+			<div class="col-8">
+					<?php
+							if (have_posts()) :
+								while (have_posts()) : the_post();
+
+								?>
+									<p><?php _e(get_the_content()); ?></p>
+								<?php
+								endwhile;
+							else :
+								_e('<p> No content found. </p>');
+							endif;
+						?>
+			</div>
+			<div class="col-4">
+				
+				
+			</div>
+
+	</div>
+
+	<div class="row each-row">
 
 
-
+	</div>
 
 </div>
-
-
-
 <?php get_footer(); ?>
