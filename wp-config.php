1b53b13b8abba917ec8e2ebad9f28cf0c84d055e
iqbalhasan24 <iqbalhasan.info@gmail.com>
2018-10-16 22:16:17 +0600

    edited gitignore

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'bangladesh_24');
+define('DB_NAME', 'bangladesh24');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'iqbal');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'iqbalhasan@2018');
+define('DB_PASSWORD', 'Mahmud_2018');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
