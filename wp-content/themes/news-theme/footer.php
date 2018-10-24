83178037954f996c8d4ae3e2d0c6ba8ebd21c423
iqbalhasan24 <iqbalhasan.info@gmail.com>
2018-10-24 22:25:54 +0600

    change footer4

--- wp-content/themes/news-theme/footer.php
+++ wp-content/themes/news-theme/footer.php
@@ -10,85 +10,85 @@
 
 <!--+++++++++++++++++Footer Section ++++++++++++++++++++++-->
 <div class="container-fluid">
-<div class="row footer-row">
-  <!-- <section class="footer" style="float: left; width: 100%;"> --> 
-		<?php if ( has_nav_menu( 'media-menu' ) ): ?>
-		    <div class="col-12 footer-social-media-menu">
-		    	<?php    			
-		 			
-				        $footer_menu_one = array(
-							'theme_location' 	=> 'media-menu',							
-							'container'       	=> 'div',
-							'menu_class'     	=> 'media-menu',
-							'before'   			=> '<ul>',
-							'after'   			=> '</ul>',
-							);
-							wp_nav_menu($footer_menu_one);
-						
-				?>
-		    </div>
-		<?php endif; ?>    
+	<div class="row footer-row">
+	  <!-- <section class="footer" style="float: left; width: 100%;"> --> 
+			<?php if ( has_nav_menu( 'media-menu' ) ): ?>
+			    <div class="col-12 footer-social-media-menu" style="width: 100%;">
+			    	<?php    			
+			 			
+					        $footer_menu_one = array(
+								'theme_location' 	=> 'media-menu',							
+								'container'       	=> 'ul',
+								'menu_class'     	=> 'media-menu',
+								/*'before'   			=> '<ul>',
+								'after'   			=> '</ul>',*/
+								);
+								wp_nav_menu($footer_menu_one);
+							
+					?>
+			    </div>
+			<?php endif; ?>    
 
 
-<?php if ( has_nav_menu( 'footer-menu-one' ) || has_nav_menu( 'footer-menu-two' ) || has_nav_menu( 'footer-menu-three' ) ): ?>
-    <div class="col-12 footer-menu-area footer-wrapper" style="margin:0;">
-    	 <div class="col-4">
-    	 		<?php
-    	 			if ( has_nav_menu( 'footer-menu-one' ) ):
-				        $footer_menu_one = array(
-							'theme_location' 	=> 'footer-menu-one',							
-							'container'       	=> 'div',
-							'menu_class'     	=> 'footer-menu footer-menu-one',
-							'before'   			=> '<ul>',
-							'after'   			=> '</ul>',
-							);
-							wp_nav_menu($footer_menu_one);
-					endif;
-				?>
-    	 </div>
-    	 
-    	 <div class="col-4">
-         		<?php
-				    if ( has_nav_menu( 'footer-menu-two' ) ):
-				        $footer_menu_two = array(
-							'theme_location' 	=> 'footer-menu-two',
-							'container'       	=> 'div',
-							'before'   			=> '<ul>',
-							'after'   			=> '</ul>',
-							'menu_class'     	=> 'col-3 footer-menu footer-menu-two',
-							);
-
-							wp_nav_menu($footer_menu_two);
-					endif;
-				?>		
-			</div>
-			<div class="col-4">
-					<?php
-						if ( has_nav_menu( 'footer-menu-three' ) ):
-					        $footer_menu_three = array(
-								'theme_location' 	=> 'footer-menu-three',
+	<?php if ( has_nav_menu( 'footer-menu-one' ) || has_nav_menu( 'footer-menu-two' ) || has_nav_menu( 'footer-menu-three' ) ): ?>
+	    <div class="col-12 footer-menu-area footer-wrapper" style="margin:0;">
+	    	 <div class="col-4">
+	    	 		<?php
+	    	 			if ( has_nav_menu( 'footer-menu-one' ) ):
+					        $footer_menu_one = array(
+								'theme_location' 	=> 'footer-menu-one',							
+								'container'       	=> 'div',
+								'menu_class'     	=> 'footer-menu footer-menu-one',
+								'before'   			=> '<ul>',
+								'after'   			=> '</ul>',
+								);
+								wp_nav_menu($footer_menu_one);
+						endif;
+					?>
+	    	 </div>
+	    	 
+	    	 <div class="col-4">
+	         		<?php
+					    if ( has_nav_menu( 'footer-menu-two' ) ):
+					        $footer_menu_two = array(
+								'theme_location' 	=> 'footer-menu-two',
 								'container'       	=> 'div',
 								'before'   			=> '<ul>',
 								'after'   			=> '</ul>',
-								'menu_class'     	=> 'footer-menu footer-menu-three',
+								'menu_class'     	=> 'col-3 footer-menu footer-menu-two',
 								);
 
-							wp_nav_menu($footer_menu_three);
+								wp_nav_menu($footer_menu_two);
 						endif;
-				?>				
-			</div>				
-    </div>
-<?php endif; ?>
+					?>		
+				</div>
+				<div class="col-4">
+						<?php
+							if ( has_nav_menu( 'footer-menu-three' ) ):
+						        $footer_menu_three = array(
+									'theme_location' 	=> 'footer-menu-three',
+									'container'       	=> 'div',
+									'before'   			=> '<ul>',
+									'after'   			=> '</ul>',
+									'menu_class'     	=> 'footer-menu footer-menu-three',
+									);
+
+								wp_nav_menu($footer_menu_three);
+							endif;
+					?>				
+				</div>				
+	    </div>
+	<?php endif; ?>
 
 
-    <div class="col-12 footer-bottom">
-      <span class="footer-copy-right1">© <?php _e(date("Y")); ?> - <?php bloginfo('name'); ?>. All Rights Reserved.
-      </span>||
+	    <div class="col-12 footer-bottom">
+	      <span class="footer-copy-right1">© <?php _e(date("Y")); ?> - <?php bloginfo('name'); ?>. All Rights Reserved.
+	      </span>||
 
-      <span class="designer1">Website Design & Development By : <a href="mailto:iqbalhasanms@gmail.com">Iqbal Mahmud Hasan</a></span>
-    </div>
-  <!-- </section> -->
-</div>
+	      <span class="designer1">Website Design & Development By : <a href="mailto:iqbalhasanms@gmail.com">Iqbal Mahmud Hasan</a></span>
+	    </div>
+	  <!-- </section> -->
+	</div>
 </div>
 <!--+++++++++++++++++Footer Section ++++++++++++++++++++++-->
 
@@ -169,6 +169,16 @@
 	margin: 0;
 	padding: 0;
 }		
+ul.media-menu{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+}
+ul.media-menu li {
+    display: inline-block;
+    margin-right: 35px;
+}
 
 .footer-social-media-menu ul li a{
 	font-size: 1.8em;
