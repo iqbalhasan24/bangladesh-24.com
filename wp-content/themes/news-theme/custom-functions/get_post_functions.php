cd9b0826e4c18e2e25ec00e355dc3950ca05f9c2
iqbalhasan24 <iqbalhasan.info@gmail.com>
2018-11-26 02:36:01 +0600

    add new home page design8

--- wp-content/themes/news-theme/custom-functions/get_post_functions.php
+++ wp-content/themes/news-theme/custom-functions/get_post_functions.php
@@ -328,6 +328,76 @@ function get_post_with_top_lead_post_and_post_thumb_on_top($post_formate, $cat_i
 /*+++++++++++++++++++++End get_post_with_top_lead_post_and_post_thumb_on_top ++++++++++++++++++++++++++++++*/
 
 
+
+
+
+
+/*+++++++++++++++++++++get_post_with_post_thumb_on_top++++++++++++++++++++++++++++++*/
+function get_post_with_post_thumb_on_top($post_formate, $cat_id, $post_no,$div_class,$title_bg_color_code){
+
+
+			 if ( has_post_format($post_formate) ) {          
+                    $args = array(
+                        'posts_per_page'  => $post_no,
+                        'cat'           => $cat_id,
+                        'orderby'       => 'date',
+                        'order'       => 'DESC',
+                    );
+
+                    $category_link = get_category_link ( $cat_id);                          
+                   
+                   	 _e("<div class='".$div_class."' style='background:#ffffff; border:1px solid #EFF5F9' >");
+		                   _e ("<div class='box-container'>");
+		                   _e ("<div class='row cat-img-containe'>");
+			                   _e("<div class='cat-img-container' style='width:100%;'> ");
+			                   			_e("<a href='".$category_link ."'><span> <h2 class='cat-title' style='background:".$title_bg_color_code."'>". get_cat_name($cat_id) ."</h2> </span></a>");
+			                           
+			                   _e("</div>");
+		                   	_e("</div>");
+                            
+
+		                  _e("<div class='row post-box-container'>");
+
+	                            $query = new WP_Query( $args );
+
+	                            	$i=0;
+
+	                            if ( $query->have_posts() ) :
+	                                while ( $query->have_posts() ) {
+	                                    $query->the_post();
+	                                    	
+	                            ?>
+	                            	<div class='col-12'>
+		                            	<div class='row'>
+			                            	<div class='single-post-box'>
+				                                <a href="<?php the_permalink();?>"><img class="img-responsive" style="width: 100%" src="<?php echo get_the_post_thumbnail_url(); ?>"></a>
+				                                <p><a href="<?php the_permalink();?>"><?php the_title(); ?></a></p>
+			                            	</div>
+			                            </div>
+			                        </div>    	
+	                           	<?php
+	                           		
+	                            	
+	                                }
+                    		
+                    		_e("</div>");
+
+                    echo "</div>";
+                    echo "</div>";
+                    endif; 
+                    wp_reset_postdata();
+            }
+
+        
+        }
+/*+++++++++++++++++++++End get_post_with_post_thumb_on_top ++++++++++++++++++++++++++++++*/
+
+
+
+
+
+
+
 /*+++++++++++++++++++++get_post_with_top_lead_post_and_post_thumb_at_left++++++++++++++++++++++++++++++*/
 function get_post_with_top_lead_post_and_post_thumb_at_left($post_formate, $cat_id, $post_no,$div_class,$title_bg_color_code){
 
