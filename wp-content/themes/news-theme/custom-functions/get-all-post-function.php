68d93c7bbe179a2ab0bf2f24aad1bc777967a971
iqbalhasan24 <iqbalhasan.info@gmail.com>
2018-12-07 05:42:27 +0600

    add prayer time on home page25

--- wp-content/themes/news-theme/custom-functions/get-all-post-function.php
+++ wp-content/themes/news-theme/custom-functions/get-all-post-function.php
@@ -165,7 +165,7 @@ function get_lead_post_by_cat_with_img_with_formate($post_formate, $cat_id, $pos
                     $category_link = get_category_link ( $cat_id);  
                     ?>
                     <div class="<?php _e($div_class);?>" style="background:<?php _e($title_bg_color_code);?> ; border:1px solid #EFF5F9; padding-top:10px;">
-                        <div class="row each-row">
+                        <div class="row">
                             <div class="col-12">
                                 <div class="category-title" >
                                     <span style=" text-transform: uppercase; background: #4db2ec;border-radius: 3px 3px 0px 0px; padding: 3px 10px;">  
